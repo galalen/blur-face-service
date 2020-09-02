7fbbd8ea90dee39fd2cec90cd9835ade61e216db
Mohammed Galalen <mohammedgalalen@gmail.com>
2020-09-02 21:26:32 +0200

    update api, requirements

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Blur-Face Service
 ###### Be Anonymous
 #### Test FastAPI async/await with computation expensive operation
+
+### Example:
+`curl -F "image=@path/to/image.png" http://localhost:8000/api/blur`
+###### Response: {"data": encoded_image_string}
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import cv2
-import io
 import base64
 import numpy as np
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import StreamingResponse, ORJSONResponse
+from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
 
 from bluring import blur_face
@@ -13,8 +12,11 @@ from bluring import blur_face
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.post("/api/blur", response_class=ORJSONResponse)
-async def handle_blur(image: UploadFile = File(...), output: str = Form(...)):
+@app.post("/api/blur")
+async def handle_blur(image: UploadFile = File(...)):
+    if not image.filename:
+        return {"error": "image not provided"}
+        
     content = await image.read()
     
     np_arr = np.fromstring(content, np.uint8)
@@ -22,10 +24,6 @@ async def handle_blur(image: UploadFile = File(...), output: str = Form(...)):
 
     output = await blur_face(img)
     res, im_png = cv2.imencode(".png", output)
-
-    # return the image output as bytes esle will return base64 
-    if str(output) == "bytes":        
-        return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
 
     encoded_img = base64.b64encode(im_png.tobytes())
     return {"data": encoded_img}
